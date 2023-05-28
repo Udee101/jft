e202ac09f2c0d875c6063cf6b54624a11ca249a5
Udeorah Frank <franklynudeorah@yahoo.com>
2023-05-28 14:28:55 +0100

    initial commit

---
+++ server/src/data-source.ts
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv"
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+dotenv.config({ path: ".env" });
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import * as express from "express"
+import * as dotenv from "dotenv"
+import { Request, Response } from "express"
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+
+dotenv.config({ path: ".env" });
+
+AppDataSource.initialize().then(() => {
+    console.log("Data Source has been initialized");
+}).catch(error => console.log(error))
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use(express.json())
+
+
+app.get("/users", async function(req:Request, res:Response) {
+    const users = await AppDataSource.getRepository(User).find()
+    res.json(users)
+})
+
+app.listen(PORT)
+
