6dfc358a1d6f681da94b65f06bda5afaaecb1fbb
Udeorah Frank <franklynudeorah@yahoo.com>
2023-05-30 11:44:54 +0100

    update

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: ".env" });
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
