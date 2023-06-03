7aa53034ae5b78835660c2f1ca18f7d0d12a5230
Udeorah Frank <franklynudeorah@yahoo.com>
2023-06-03 10:28:06 +0100

    modify categories section

--- client/src/components/CategoryBox.vue
+++ client/src/components/CategoryBox.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="category-box">
-    <img :src="category.imgSrc" alt="image" class="py-1">
-    <p class="mb-1 text-md ">{{ category.title }}</p>
+  <div 
+    class="category-box scale"
+    :class="category.color"
+  >
+    <p class="mb-1 text-md">{{ category.title }}</p>
   </div>
 </template>
 
@@ -16,4 +18,25 @@
 </script>
 
 <style scoped>
+.category-box>p{
+  max-width: 200px;
+}
+.border-left-color-1 {
+  border-left: 5px solid #f77288;
+}
+.border-left-color-2 {
+  border-left: 5px solid #bd2cb1;
+}
+.border-left-color-3 {
+  border-left: 5px solid #5959ed;
+}
+.border-left-color-4 {
+  border-left: 5px solid #ffff00;
+}
+.border-left-color-5 {
+  border-left: 5px solid #0bba0b;
+}
+.border-left-color-6 {
+  border-left: 5px solid #19e0b2;
+}
 </style>
