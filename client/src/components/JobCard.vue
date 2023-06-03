1565145ac8ad37dd09d9eb93d09825a5d7daf80b
Udeorah Frank <franklynudeorah@yahoo.com>
2023-06-03 16:01:10 +0100

    modify job card UI

--- client/src/components/JobCard.vue
+++ client/src/components/JobCard.vue
@@ -1,43 +1,26 @@
 <template>
-  <div class="component-wrapper">
-    <div class="job-box scale">
+  <div  class="component-wrapper">
+    <router-link :to="{name: 'listing.id', params: { listingId: job.id }}" class="job-box scale text-black">
 
       <div class="comp-img">
-        <img :src="CompImg" alt="">
+        <img :src="CompImg" alt="job-svg" width="80" height="80">
       </div>
 
       <div class="detail-wrapper">
-        <p class="comp-title text-md mb-0-5">{{ job.title }}</p>
-
-        <div class="job-type">
-          <p class="type text-base">Fulltime.</p>
-          <p class="job-loc"><f-a-i icon="fas fa-location-dot" class="text-color-9" /> {{ job.location }}</p>
-        </div>
-
-        <p class="comp-name text-md my-1">{{ job.company }}</p>
-
-        <div class="comp-tags mb-1">
-          <span>tags: </span>
-          <div v-for="(tag, i) in formatedJobTags" :key="i">
-            <p class="text-sm text-color-3">{{ tag }}</p>
-          </div>
-        </div>
-
-        <router-link :to="{name: 'listing', params: { listingId: job.id }}" class="text-color-4 view-job hover-opacity">View Job</router-link>
+        <p class="comp-title text-md">{{ job.title }}</p>
+        <p class="comp-name text-md text-color-3 my-0-5">{{ job.company }}</p>
+        <p class="job-loc text-color-2">
+          <f-a-i icon="fas fa-location-dot" /> 
+          {{ job.location }}
+        </p>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
 <script>
-import CompImg from '../assets/img/j4logo.svg'
+import CompImg from '../assets/img/patternpad.svg'
   export default {
-    computed: {
-      formatedJobTags() {
-        const tagsArray = this.job.tags.split(',');
-        return tagsArray;
-      }
-    },
     data() {
       return {
         CompImg
