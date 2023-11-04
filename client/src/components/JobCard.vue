a58f891c388792acc659c84aa76ad407b462aa85
Udeorah Frank <franklynudeorah@gmail.com>
2023-11-04 22:14:16 +0100

    add job post date

--- client/src/components/JobCard.vue
+++ client/src/components/JobCard.vue
@@ -14,6 +14,7 @@
           {{ job.location }}
         </p>
       </div>
+    <p :class="`date-posted ${styleDaysPosted}`">{{ getTimeDifferenceSinceWhenPosted }} {{ getDateText(getTimeDifferenceSinceWhenPosted) }} </p>
     </router-link>
   </div>
 </template>
@@ -25,7 +26,36 @@ import { toSvg } from "jdenticon"
     computed: {
       setImg(){
         return toSvg(this.job.company, 80)
+      },
+
+      getTimeDifferenceSinceWhenPosted() {
+        let today = new Date();
+        let jobDate = new Date(this.job.created_at);
+
+        let differenceInTime = today.getTime() - jobDate.getTime();
+        return Math.floor(differenceInTime / (1000 * 3600 * 24))
+      },
+
+      styleDaysPosted() {
+        if (this.getTimeDifferenceSinceWhenPosted <= 7 ) {
+          return "new"
+        } else if (this.getTimeDifferenceSinceWhenPosted <= 14 ) {
+          return "recent"
+        }
+        return "old"
       }
+    },
+
+    methods: {
+      getDateText(days) {
+        if (days < 1) {
+          return "Today"
+        } else if ( days === 1 ) {
+          return "day ago"
+        } else {
+          return "days ago"
+        }
+      },
     },
 
     props: {
@@ -37,4 +67,24 @@ import { toSvg } from "jdenticon"
 </script>
 
 <style scoped>
+.date-posted {
+  position: absolute;
+  bottom: 0.5rem;
+  right: 0.5rem;
+  border-radius: 0.7rem;
+  padding: 0 0.5rem 0.25rem;
+  font-size: 0.85rem;
+}
+.new {
+  background: #c5f0c57a;
+  color: #159615;
+}
+.recent {
+  background: #f3e59573;
+  color: #ccb054;
+}
+.old {
+  background: #dddddd81;
+  color: #8f8d8d;
+}
 </style>
